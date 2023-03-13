5c19fa13a96423a69a469601db6a154e3a11fe2b
Jakubpatelski <jakubpatelski24@gmail.com>
2023-03-13 14:05:36 +0100

    Initial Commit

---
+++ LogIn_Birds/LogIn_BirdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogIn_BirdsApp.swift
+//  LogIn_Birds
+//
+//  Created by Jakub Patelski on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LogIn_BirdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
